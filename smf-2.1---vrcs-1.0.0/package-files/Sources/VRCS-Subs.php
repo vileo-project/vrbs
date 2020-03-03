10d00120b026e6f877eeebf7b127bfb1c486f9c0
Vital78 <vital78.od@gmail.com>
2020-03-03 15:36:00 +0200

    add cookie registration

--- smf-2.1---vrcs-1.0.0/package-files/Sources/VRCS-Subs.php
+++ smf-2.1---vrcs-1.0.0/package-files/Sources/VRCS-Subs.php
@@ -7,7 +7,10 @@ db_extend('packages');
 
 function regReferral($memberID){
 	global $smcFunc;
-	
+	$cookieValue = getReflinkCookie();
+
+	$smcFunc['db_query']('', 'CALL smf_vrcs_addmembers ('.(int)$memberID.','.(int)$cookieValue.')');
+
 	while (!$smcFunc['db_insert']('ignore',
 		'{db_prefix}vrcs_link',
 		array('id_member' => 'int', 'id_reflink' => 'int'),
@@ -15,6 +18,32 @@ function regReferral($memberID){
 		array('id_member', 'id_reflink'),
 		1
 		));
+}
+
+	// Set/delete reflink cookie file.
+function setReflinkCookie($vrcsCookieValue){
+	global $vrcsCookieName;
+
+	$expires = !empty($vrcsCookieValue) ? time() + 15768000 : 0;
+	$cookie_url = url_parts(!empty($modSettings['localCookies']), !empty($modSettings['globalCookies']));
+	smf_setcookie($vrcsCookieName, $vrcsCookieValue, $expires, $cookie_url[1], $cookie_url[0], 0, 0);
+}
+
+	// Get reflink cookie.
+function getReflinkCookie(){
+	global $vrcsCookieName;
+
+	$vrcsCookieValue = 0;
+	if (isset($_COOKIE[$vrcsCookieName]))
+		$vrcsCookieValue = (int)$_COOKIE[$vrcsCookieName];
+	return $vrcsCookieValue;
+}
+
+	// Set reflink cookie if not present yet.
+function reflinkCookie($refLink){
+	if (!empty(getReflinkCookie()))
+		return;
+	setReflinkCookie($refLink);
 }
 
 ?>
